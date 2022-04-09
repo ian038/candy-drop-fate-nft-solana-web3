6dce78c55d721892f9b5855f5dba86924aff218b
ian038 <ianphua3@gmail.com>
2022-04-09 19:21:52 -0400

    Created functionality to connect phantom wallet to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,17 +7,31 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null)
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
 
       if (solana && solana.isPhantom) {
-        console.log('Phantom wallet found!');
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log('Connected with Public Key:', response.publicKey.toString())
+        setWalletAddress(response.publicKey.toString());
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
       }
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
     }
   };
 
@@ -35,6 +49,14 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {!walletAddress && (
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWallet}
+            >
+              Connect to Wallet
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
